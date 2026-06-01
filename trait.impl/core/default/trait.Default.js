cdeac748ce4e52417bc95072553d062095c74397
yonas <yonas@users.noreply.github.com>
2026-06-01 04:22:13 +0000

    deploy: eebba7c737fe67fe979f1db7454f45646edb9c0c

---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["adguardian",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"adguardian/fetch/fetch_query_log/struct.Query.html\" title=\"struct adguardian::fetch::fetch_query_log::Query\">Query</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"adguardian/fetch/fetch_query_log/struct.QueryResponse.html\" title=\"struct adguardian::fetch::fetch_query_log::QueryResponse\">QueryResponse</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"adguardian/fetch/fetch_query_log/struct.Question.html\" title=\"struct adguardian::fetch::fetch_query_log::Question\">Question</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"adguardian/fetch/fetch_stats/struct.StatsResponse.html\" title=\"struct adguardian::fetch::fetch_stats::StatsResponse\">StatsResponse</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"adguardian/fetch/fetch_status/struct.StatusResponse.html\" title=\"struct adguardian::fetch::fetch_status::StatusResponse\">StatusResponse</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[1626]}
